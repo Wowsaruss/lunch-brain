d62b3c0b7dde8ecbc8b0ad5c91b8c6cd0a82acd6
Wowsaruss <russell.hayes.dev@gmail.com>
2020-01-06 08:01:21 -0700

    res message

--- src/slack.js
+++ src/slack.js
@@ -6,10 +6,10 @@ async function slackTest(req, res) {
   try {
     console.log(req);
 
-    const test = await Slack.api.test({ nice: 1 });
-    const auth = await bot.api.test({ hyper: 'card' });
+    // const test = await Slack.api.test({ nice: 1 });
+    // const auth = await bot.api.test({ hyper: 'card' });
 
-    return res.status(200).json({ ...test, ...auth });
+    return res.status(200).json('this is complete');
   } catch (e) {
     console.log(e);
   }
