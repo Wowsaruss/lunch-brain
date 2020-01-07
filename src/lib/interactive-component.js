8b99afd4972ee76a4c572541c78e5521f1bf9eaf
Wowsaruss <russell.hayes.dev@gmail.com>
2020-01-07 08:23:54 -0700

    test

--- src/lib/interactive-component.js
+++ src/lib/interactive-component.js
@@ -1,46 +1,10 @@
 async function interactiveComponent(req, res) {
   console.log('!!!!!!!!!!!HIT2', req.body);
-  return res.status(200).json([
-    {
-      type: 'section',
-      block_id: 'section678',
-      text: {
-        type: 'mrkdwn',
-        text: 'Pick items from the list'
-      },
-      accessory: {
-        action_id: 'text1234',
-        type: 'multi_static_select',
-        placeholder: {
-          type: 'plain_text',
-          text: 'Select items'
-        },
-        options: [
-          {
-            text: {
-              type: 'plain_text',
-              text: '*this is plain_text text*'
-            },
-            value: 'value-0'
-          },
-          {
-            text: {
-              type: 'plain_text',
-              text: '*this is plain_text text*'
-            },
-            value: 'value-1'
-          },
-          {
-            text: {
-              type: 'plain_text',
-              text: '*this is plain_text text*'
-            },
-            value: 'value-2'
-          }
-        ]
-      }
-    }
-  ]);
+  return res.status(200).json({
+    type: 'image',
+    image_url: 'http://placekitten.com/700/500',
+    alt_text: 'Multiple cute kittens'
+  });
 }
 
 module.exports = interactiveComponent;
