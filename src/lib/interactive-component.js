d4cb0c60388fa9c74e1aa5ecd1d5e94f5757cb4b
Wowsaruss <russell.hayes.dev@gmail.com>
2020-01-07 08:48:49 -0700

    json

--- src/lib/interactive-component.js
+++ src/lib/interactive-component.js
@@ -1,6 +1,6 @@
 async function interactiveComponent(req, res) {
   console.log('!!!!!!!!!!!HIT2', req.body);
-  return res.status(200).send({
+  return res.status(200).json({
     type: 'image',
     image_url: 'http://placekitten.com/700/500',
     alt_text: 'Multiple cute kittens'
