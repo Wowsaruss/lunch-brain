bc57eef5a67ac6e53df76cc66483e75d07ab2f2b
Wowsaruss <russell.hayes.dev@gmail.com>
2020-01-07 08:46:30 -0700

    send

--- src/lib/action.js
+++ src/lib/action.js
@@ -1,6 +1,6 @@
 async function interactiveComponent(req, res) {
   console.log('!!!!!!!!!!!HIT1', req.body);
-  return res.status(200).json({
+  return res.status(200).send({
     type: 'image',
     image_url: 'http://placekitten.com/700/500',
     alt_text: 'Multiple cute kittens'
